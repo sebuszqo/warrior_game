d205a4d92623340064cbf28381367835842a6d51
sebuszqo <sekn.kn@gmail.com>
2022-12-22 19:45:02 +0100

    Stage 3 configuration of index.ts file with express things etc

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
   - V1: 
     - ~~konfiguracja środowiska~~
     - ~~instalacja potrzebnych paczek i konfiguracja TypeScript oraz skryptów pomocniczych~~
-    - Konfiguracja Expressa oraz Folderu publiczny pliki statyczne
+    - ~~Konfiguracja Expressa oraz Folderu publiczny pliki statyczne~~
     - Ogólna struktura routerów - ścieżek - zaplanowanie ścieżek, jakie się pojawią
     - zaplanowanie widoków - ogólna struktura widoków
     - Rekordy 
--- index.ts
+++ index.ts
@@ -1,7 +1,44 @@
 import * as express from "express";
+import 'express-async-errors';
 import * as methodOverride from "method-override";
+import {static as expressStatic , urlencoded} from "express";
+import {engine} from "express-handlebars";
 
+// creating an express app
 const app = express();
 
+// methodOverride to use with PATCH PUT etc.
 app.use(methodOverride('_method'));
+
+// I will use it cuz we will get our data through forms
+app.use(urlencoded({
+    extended: true,
+}));
+
+// I will be serving static data from dir named 'public'
+// app.use(expressStatic("__dir"))
+app.use(expressStatic("public"));
+
+// layouts engine - I will use express handlebars
+app.engine('.hbs', engine({
+    extname: '.hbs',
+    //helpers: ???,
+}));
+
+// setting my view engine to .hbs
+app.set('view engine', '.hbs')
+
+app.get('/', (req,res)=>{
+    res.send('Hello!');
+});
+
+//handling errors
+// app.use(handleError)
+
+// app is listening on port 3000 - console log to click every time when I need it - I do not have to copy it ;)
+app.listen(3000, 'localhost', () =>{
+    console.log("Listening on http://localhost:3000");
+});
+
+
 
