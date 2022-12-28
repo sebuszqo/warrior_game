016d17fb6c1061fff71244b67ebf5c642be897b2
sebuszqo <sekn.kn@gmail.com>
2022-12-28 22:27:33 +0100

    Adding new warrior first stage - added form to do it and start dealing with data

--- routers/warrior.ts
+++ routers/warrior.ts
@@ -1,14 +1,15 @@
-import { Router } from "express";
+import {Router} from "express";
 
 // making a 'warrior' router to handle our warrior ;)
 export const warriorRouter = Router();
 
 warriorRouter
     // form to add a new warrior
-    .get('/add-form', (req,res)=>{
+    .get('/add-form', (req, res) => {
         res.render("warrior/add-form");
     })
     // post to post new warrior
-    .post('/', (req, res)=>{
+    .post('/', (req, res) => {
+        console.log(req.body)
         res.render("warrior/warrior-added");
     })
