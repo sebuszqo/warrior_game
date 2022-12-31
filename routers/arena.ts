4e748e1fb0638e03382590245c7e0a46eac69433
sebuszqo <sekn.kn@gmail.com>
2022-12-31 15:36:48 +0100

    Added choosing warriors to make a fight and validations of daredevils - 2 cases: warriors are the same, warriors are not exist anymore

--- routers/arena.ts
+++ routers/arena.ts
@@ -1,15 +1,31 @@
-import { Router } from "express";
+import {Router} from "express";
+import {WarriorRecord} from "../records/warrior.record";
+import {ValidationError} from "../utils/errors";
 
 // making a 'arena' router
 export const arenaRouter = Router();
 
 arenaRouter
     // form to make a fight
-    .get('/fight-form', (req,res)=>{
-        res.render("arena/fight-form");
+    .get('/fight-form', async (req, res) => {
+        const warriors = await WarriorRecord.listAll();
+        res.render("arena/fight-form", {
+            warriors
+        });
     })
     // post to start a fight
-    .post('/fight', (req, res)=>{
-        res.send("arena/fight");
+    .post('/fight', async (req, res) => {
+        const {warrior1: warrior1Id, warrior2: warrior2Id} = req.body
+        const warrior1 = await WarriorRecord.getOneWarrior(warrior1Id);
+        const warrior2 = await WarriorRecord.getOneWarrior(warrior2Id);
+        // making validations !
+        if (warrior1Id === warrior2Id) {
+            throw new ValidationError("You have chosen the same warrior for the fight, he cannot fight himself!")
+        }
+        if (!warrior1 || !warrior2) {
+            throw new ValidationError("We couldn't find opponent for fight, try again ")
+        }
+
+        res.render("arena/fight");
     })
 
