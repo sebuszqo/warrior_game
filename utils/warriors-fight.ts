0feb4dc9c0f62b4aa39676207c80371f86efb92b
sebuszqo <sekn.kn@gmail.com>
2023-01-01 15:54:55 +0100

    First part of fight algorithm - to add - logs of fight

--- utils/warriors-fight.ts
+++ utils/warriors-fight.ts
@@ -1,2 +1,48 @@
-export const fight = () => {
-}
+import {WarriorRecord} from "../records/warrior.record";
+
+export const fight = (warrior1: WarriorRecord, warrior2: WarriorRecord): string[] => {
+    const log: string[] = [];
+
+
+    // making 2 warrior tmp stats object to change and use tmp data as
+    //for example current health without changing anything in main object
+    const warrior1TmpStats = {
+        hp: warrior1.stamina * 10,
+        dp: warrior1.defence,
+        warrior: warrior1
+    };
+
+    const warrior2TmpStats = {
+        hp: warrior2.stamina * 10,
+        dp: warrior2.defence,
+        warrior: warrior2
+    };
+    let attacker = warrior1TmpStats;
+    let defender = warrior2TmpStats
+
+    // loop algorithm to make a fight
+    do {
+        const attackStrength = attacker.warrior.power
+
+        // DP: 5, HP: 5, attackStrength: 7
+
+        if (defender.dp + defender.warrior.agility > attackStrength) {
+            defender.dp -= attackStrength
+
+            // DP: -2, HP: 5
+
+            if (defender.dp < 0) {
+                defender.hp += defender.dp
+
+                // DP = 5 - -(-2) = 5 - 2 = 3
+            }
+        }
+
+        // reversing  defender with attacker by using destructuring arrays
+        [defender, attacker] = [attacker, defender];
+
+        //  they are fighting until one of them is dead
+    } while (warrior1TmpStats.hp > 0 && warrior2TmpStats.hp > 0)
+
+    return log;
+};
